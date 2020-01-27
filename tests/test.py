0314b0a2a99405535f64a6d9d8e7b728dfb2db1d
Yusuke Kambara <yusukekambara@gmail.com>
2020-01-27 16:18:00 +0900

    Added the test module for utils function

--- tests/test.py
+++ tests/test.py
@@ -1,8 +1,10 @@
 import unittest
 from tests import test_main
+from tests.utils import test_utils
 
 
 def suite():
   suite = unittest.TestSuite()
   suite.addTests(unittest.makeSuite(test_main.TestMain))
+  suite.addTests(unittest.makeSuite(test_utils.TestUtils))
   return suite
