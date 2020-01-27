b2ee69d07f12e483b4aee78a7a457c3acfc50c99
Yusuke Kambara <yusukekambara@gmail.com>
2020-01-27 18:27:16 +0900

    Added the test module for datasource.jar module

---
+++ tests/datasource/jra/test_io.py
@@ -0,0 +1,37 @@
+import unittest
+import zipfile
+from datetime import datetime
+from src.datasource.jra import io as jra
+
+class TestIo(unittest.TestCase):
+    """Test class for testing the jra datasource
+    
+    Arguments:
+        unittest {[type]} -- [description]
+    """
+    
+    def test_get_race_name_list_with_invalid_argument_year(self):
+        """Testing to occurred error if the argument's year is invaild
+        """
+        with self.assertRaises(zipfile.BadZipFile):
+            jra.get_race_name_list(9999, False)
+        with self.assertRaises(zipfile.BadZipFile):
+            jra.get_race_name_list(9999, True)
+
+    def test_get_race_name_list(self):
+        """Testing to be able to get the raca name data in this year
+        """
+        current_year = datetime.now().year
+        race_name_data = jra.get_race_name_list(current_year, False)
+        assert len(race_name_data) != 0
+        assert race_name_data[0]["date"].year == current_year
+        assert race_name_data[0]["title"] != ""
+
+    def test_get_race_name_list_with_place_optino(self):
+        """Testing to be able to get the place to open the event
+        """
+        current_year = datetime.now().year
+        race_name_data = jra.get_race_name_list(current_year, True)
+        assert len(race_name_data) != 0
+        assert race_name_data[0]["date"].year == current_year
+        assert "競馬" in race_name_data[0]["title"]
--- tests/test.py
+++ tests/test.py
@@ -2,6 +2,7 @@ import unittest
 from tests import test_main
 from tests.utils import test_utils
 from tests.commands import test_schedule
+from tests.datasource.jra import test_io as test_jra
 
 
 def suite():
@@ -9,4 +10,5 @@ def suite():
   suite.addTests(unittest.makeSuite(test_main.TestMain))
   suite.addTests(unittest.makeSuite(test_utils.TestUtils))
   suite.addTests(unittest.makeSuite(test_schedule.TestSchedule))
+  suite.addTests(unittest.makeSuite(test_jra.TestIo))
   return suite
