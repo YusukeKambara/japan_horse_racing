cc014027f942e5ac5a3a993f11974ef890982b37
Yusuke Kambara <yusukekambara@gmail.com>
2020-01-27 18:11:39 +0900

    Added the test module for commands.schedule module

---
+++ tests/commands/test_schedule.py
@@ -0,0 +1,37 @@
+import unittest
+import zipfile
+from datetime import datetime
+from src.commands import schedule as commands_schedule
+
+class TestSchedule(unittest.TestCase):
+    """Test class for testing the commands.schedule module
+    
+    Arguments:
+        unittest {[type]} -- [description]
+    """
+    
+    def test_get_with_invaild_year_race_name(self):
+        """Testing to occurred error if the argument's year is invaild
+        """
+        with self.assertRaises(zipfile.BadZipFile):
+            commands_schedule.get(9999, False)
+        with self.assertRaises(zipfile.BadZipFile):
+            commands_schedule.get(9999, True)
+
+    def test_get_of_race_name(self):
+        """Testing to be able to get the raca name data in this year
+        """
+        current_year = datetime.now().year
+        race_name_data = commands_schedule.get(current_year, False)
+        assert len(race_name_data) != 0
+        assert race_name_data[0]["date"].year == current_year
+        assert race_name_data[0]["title"] != ""
+
+    def test_get_of_place(self):
+        """Testing to be able to get the place to open the event
+        """
+        current_year = datetime.now().year
+        race_name_data = commands_schedule.get(current_year, True)
+        assert len(race_name_data) != 0
+        assert race_name_data[0]["date"].year == current_year
+        assert "競馬" in race_name_data[0]["title"]
--- tests/test.py
+++ tests/test.py
@@ -1,10 +1,12 @@
 import unittest
 from tests import test_main
 from tests.utils import test_utils
+from tests.commands import test_schedule
 
 
 def suite():
   suite = unittest.TestSuite()
   suite.addTests(unittest.makeSuite(test_main.TestMain))
   suite.addTests(unittest.makeSuite(test_utils.TestUtils))
+  suite.addTests(unittest.makeSuite(test_schedule.TestSchedule))
   return suite
